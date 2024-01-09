7ac94ec41aadb93fb00f529bdbf21dbe3346a1ff
Mia Cain <55184695+MiaCain@users.noreply.github.com>
2024-01-09 17:20:30 +0100

    Begin kitchen imp, add texture scroller

---
+++ Assets/_Wormcatcher/Scripts/Visuals/TextureScroller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureScroller : MonoBehaviour
+{
+    [SerializeField] private float ySpeed;
+    [SerializeField] private float xSpeed;
+    [SerializeField] private Material mat;
+
+    
+    void FixedUpdate()
+    {
+        mat.mainTextureOffset += new Vector2(xSpeed, ySpeed);
+
+        if (Mathf.Abs(mat.mainTextureOffset.x) >= 10)
+        {
+            mat.mainTextureOffset = new Vector2(0, mat.mainTextureOffset.y);
+        }
+        if (Mathf.Abs(mat.mainTextureOffset.y) >= 10)
+        {
+            mat.mainTextureOffset = new Vector2(mat.mainTextureOffset.x, 0);
+        }
+    }
+}
