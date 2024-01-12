289765a0a01770a975377b56dec1278c77147eb8
lilithisa <101096537+lilithisa@users.noreply.github.com>
2024-01-12 09:13:33 +0100

    AudioOneshot

---
+++ Assets/_Wormcatcher/Scripts/Audio/AudioOneShotCaller.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class AudioOneShotCaller : MonoBehaviour
+{
+    public void RequestplayOneShot(EventReference sound)
+    {
+        AudioManager.Instance.PlayOneShot(sound, this.transform.position);
+    }
+}
