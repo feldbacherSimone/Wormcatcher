af6ba0b979c240b2ff06a951dcef85b0b3d62704
feldbacherSimone <simone.feldbacher@gmail.com>
2024-01-17 16:17:12 +0100

    add main menu logic

---
+++ Assets/_Wormcatcher/Scripts/UI/MainMenu.cs
@@ -0,0 +1,36 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Wormcatcher.Scripts.UI
+{
+    public class MainMenu : MonoBehaviour
+    {
+        [SerializeField] private Button quitButton; 
+        [SerializeField] private Button playButton; 
+        [SerializeField] private Button settingButton; 
+        [SerializeField] private Button controlsButton;
+
+        private void Start()
+        {
+            quitButton.onClick.AddListener(() =>
+            {
+                Application.Quit();
+            });
+            playButton.onClick.AddListener(() =>
+            {
+                SceneLoader.SwitchScene(3);
+                Cursor.lockState = CursorLockMode.Locked;
+            });
+            settingButton.onClick.AddListener(() =>
+            {
+                Debug.Log("settings Pressed ");
+            });
+            
+            controlsButton.onClick.AddListener(() =>
+            {
+                Debug.Log("controlls Pressed ");
+            });
+        }
+    }
+}
