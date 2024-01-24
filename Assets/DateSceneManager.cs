2e2ecc4c4cf9f31423b0529cb99837d96a54eee9
feldbacherSimone <simone.feldbacher@gmail.com>
2024-01-24 17:12:15 +0100

    add date scene manager

--- Assets/DateSceneManager.cs
+++ Assets/DateSceneManager.cs
@@ -1,7 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using _Wormcatcher.Scripts;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using Yarn.Unity;
 
 public class DateSceneManager : MonoBehaviour
 {
+   [SerializeField] private float timeToStart = 1f; 
+   [SerializeField] private float timeToEnd = 2f;
+
+
+    [SerializeField] private DialogueRunner dialogueRunner;
+
+    IEnumerator StartDialogue()
+    {
+        yield return new WaitForSeconds(timeToStart);
+        dialogueRunner.StartDialogue("V2");
+    }
+
+    public void EndVignette()
+    {
+        StartCoroutine(OnEndDialogue());
+    }
+
+    IEnumerator OnEndDialogue()
+    {
+        yield return new WaitForSeconds(timeToEnd);
+        SceneLoader.SwitchScene(3);
+    }
 }
