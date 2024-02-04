d4c8a25c484d9673fe0110560a97d08cb5cf319f
feldbacherSimone <simone.feldbacher@gmail.com>
2024-02-04 14:25:11 +0100

    add flashlight

---
+++ Assets/Vignette5Manager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using _Wormcatcher.Scripts;
+using _Wormcatcher.Scripts.GameplayManagers;
+using _Wormcatcher.Scripts.Inputs;
+using _Wormcatcher.Scripts.Interaction.SceneObjects;
+using UnityEngine;
+
+public class Vignette5Manager : VignetteManager
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private Transform[] spawnPositions;
+    [SerializeField] private PlayerMovement playerMovement;
+    [SerializeField] private CanvasGroup mouseIcon;
+    private void Awake()
+    {
+        if (overrideSpawnPoints)
+        {
+            playerMovement.Active = true;
+            return;
+        }
+        
+        player.transform.position = spawnPositions[PlayerData.V1Progress].position;
+        player.transform.rotation = spawnPositions[PlayerData.V1Progress].rotation;
+        playerMovement.Active = true;
+        ShowLeftClickIcon();
+    }
+
+    public override void ChangeToApartment()
+    {
+        throw new System.NotImplementedException();
+    }
+    
+    public void ShowLeftClickIcon()
+    {
+        StartCoroutine(ShowLeftClickIconInternal());
+    }
+    
+    IEnumerator ShowLeftClickIconInternal()
+    {
+        yield return new WaitForSeconds(5);
+   
+        StartCoroutine(TextEffects.FadeIn(mouseIcon, 2.0f,
+            () =>
+            {
+                StartCoroutine(TextEffects.FadeOut(mouseIcon, 2, () =>
+                {
+                    
+                }));
+            }));
+    }
+}
